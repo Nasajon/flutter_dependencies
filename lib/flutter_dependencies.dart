692e160c92d8ef70a2e33f73dfad22fe1fc50114
João Silva <joaosilva@nasajon.com.br>
2022-11-16 13:45:17 -0300

    Fix currency_text_input_formatter export

--- lib/flutter_dependencies.dart
+++ lib/flutter_dependencies.dart
@@ -33,4 +33,4 @@ export 'package:slang_flutter/slang_flutter.dart';
 export 'package:retrofit/retrofit.dart';
 export 'package:lottie/lottie.dart';
 export 'package:auto_size_text/auto_size_text.dart';
-export 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+export 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
